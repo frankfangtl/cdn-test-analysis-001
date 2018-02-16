41df3df0113a64e20d39ee04785b174001092680
leo <chenlintao@gmail.com>
2018-02-16 14:13:48 +0000

    Adding test-case-1 test-case-2 scripts
    Signed-off-by: root <root@ip-172-31-12-100.ap-northeast-1.compute.internal>

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cdn-test-analysis-001
+
+
+
+## Sample 
+- s3://aws-china-cs-sa-group/sample-data/cloudfront-logs/ctrip-201802-test/aws-test/
---
+++ client-test/test-case-2.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+
+BASENAME=test-case-2
+INTERVAL=30
+
+function do_log() 
+{
+LOG=$1
+
+T=$(date '+%Y/%m/%d %T')
+
+#echo "== CloudFront =="
+echo -n $T >> $LOG
+echo -n ",AWS-CF," >> $LOG
+E=http://aw-apiproxy.ctrip.com/api/framework/images4/target/nocache
+
+curl -o /dev/null -s -w %{time_namelookup},%{time_connect},%{time_starttransfer},%{time_total},%{speed_download},%{http_code} -X POST -F file=@nocache.txt "$E?domain=l3&region=%E6%9C%AA%E7%9F%A5&type=ec2&uuid=9ea4e0b8-68a8-4ec5-b1c7-9e6942014ccb" -H "Expect: " >> $LOG
+echo >> $LOG
+
+#echo "==Akamai=="
+echo -n $T >> $LOG
+echo -n ",Akamai," >> $LOG
+E=http://apiproxy.ctrip.com/api/framework/images4/target/nocache
+curl -o /dev/null -s -w %{time_namelookup},%{time_connect},%{time_starttransfer},%{time_total},%{speed_download},%{http_code} -X POST -F file=@nocache.txt "$E?domain=l3&region=%E6%9C%AA%E7%9F%A5&type=ec2&uuid=9ea4e0b8-68a8-4ec5-b1c7-9e6942014ccb" -H "Expect: " >> $LOG
+echo >> $LOG
+
+#echo "==Limelight=="
+echo -n $T >> $LOG
+echo -n ",Limelight," >> $LOG
+E=http://lm-apiproxy.ctrip.com/api/framework/images4/target/nocache
+curl -o /dev/null -s -w %{time_namelookup},%{time_connect},%{time_starttransfer},%{time_total},%{speed_download},%{http_code} -X POST -F file=@nocache.txt "$E?domain=l3&region=%E6%9C%AA%E7%9F%A5&type=ec2&uuid=9ea4e0b8-68a8-4ec5-b1c7-9e6942014ccb" -H "Expect: " >> $LOG
+echo >> $LOG
+
+#echo "==Level3=="
+echo -n $T >> $LOG
+echo -n ",Ayaka(level3)," >> $LOG
+E=http://l3-apiproxy.ctrip.com/api/framework/images4/target/nocache
+curl -o /dev/null -s -w %{time_namelookup},%{time_connect},%{time_starttransfer},%{time_total},%{speed_download},%{http_code} -X POST -F file=@nocache.txt "$E?domain=l3&region=%E6%9C%AA%E7%9F%A5&type=ec2&uuid=9ea4e0b8-68a8-4ec5-b1c7-9e6942014ccb" -H "Expect: " >> $LOG
+echo >> $LOG
+
+#echo "==CloudFlare=="
+echo -n $T >> $LOG
+echo -n ",CloudFlare," >> $LOG
+E=http://cf-apiproxy.ctrip.com/api/framework/images4/target/nocache
+curl -o /dev/null -s -w %{time_namelookup},%{time_connect},%{time_starttransfer},%{time_total},%{speed_download},%{http_code} -X POST -F file=@nocache.txt "$E?domain=l3&region=%E6%9C%AA%E7%9F%A5&type=ec2&uuid=9ea4e0b8-68a8-4ec5-b1c7-9e6942014ccb" -H "Expect: " >> $LOG
+echo >> $LOG
+
+}
+
+
+## Main ##
+mkdir -p logs/$BASENAME/
+
+#echo "Date,Provider,time_namelookup,time_connect,time_starttransfer,time_total,speed_download,http_code" > $LOG
+
+while  [ 1 ]; do
+	LOG_FILE="logs/$BASENAME/$BASENAME.$(date +%Y-%m-%d).csv"
+
+	do_log ${LOG_FILE}
+##	exit 0
+
+
+	sleep $INTERVAL
+done
+
+exit 0
